62df685e45f82bd4a013a494f9f7ec9cda3a0097
Juleen Graham <jugraham@vassar.edu>
2016-05-08 23:43:09 -0400

    Sync Work

---
+++ TechDiversity.js
@@ -0,0 +1,9 @@
+
+//For the sign up button that opens the page to sign up
+function openSignUp () {
+	window.open("SignUp.html");
+}
+
+function openGHCWebsite () {
+	window.open("http://ghc.anitaborg.org/2016-student-academic/scholarships/?_ga=1.170681831.634347850.1459694630");
+}
---
+++ headerfooter.js
@@ -0,0 +1,4 @@
+window.onload = function () {
+	document.getElementById('header').innerHTML = "<h1>TechDiversity</h1><h3>Striving for a more diverse tech industry!</h3>";
+	document.getElementById('footer').innerHTML = "<h1>Insert footer here</h1>";
+}
